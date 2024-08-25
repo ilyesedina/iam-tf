5ddfe75f5c13fdcfd4dc5d17d8fcbafe4a4355e8
Edina Ilyes <edina.ilyes@lego.com>
2024-08-25 23:35:47 +0200

    test tags

--- iam/environments/sandbox/providers.tf
+++ iam/environments/sandbox/providers.tf
@@ -3,17 +3,14 @@ provider "aws" {
   profile = "lego-qsc-${local.environment}:Admin"
 
   default_tags {
-    tags = module.cloud_tags.tags
+    tag_dataclassification               = "Green"
+    tag_environment                      = reverse(split("/", abspath(path.root)))[0]
+    tag_ManagedWith                      = "Terraform"
+    tag_purpose                          = "Learning"
+    tags_override = {
+      "repository" = "https://github.com/ilyesedina/iam-tf"
+      "Owner" : "Ilyes Edina"
+    }
   } 
 }
 
-module "cloud_tags" {
-  tag_dataclassification               = "Green"
-  tag_environment                      = reverse(split("/", abspath(path.root)))[0]
-  tag_ManagedWith                      = "Terraform"
-  tag_purpose                          = "Learning"
-  tags_override = {
-    "repository" = "https://github.com/ilyesedina/iam-tf"
-    "Owner" : "Ilyes Edina"
-  }
-}
